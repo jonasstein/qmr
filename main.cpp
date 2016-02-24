a80f20e456bf1c7579b2abae56a8a0eed4911275
Jonas Stein <news@jonasstein.de>
2016-02-25 00:12:21 +0100

    fix main: get -> show

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main () {
   lmfile* limo;
   limo = new lmfile("180sec_2chan_5kHz_pm_4kHz_FM_4Hz_Trigger1Hz.mdat");
   
-  std::cout << limo->getfilesize() << std::endl ; 
+  std::cout << limo->showfilesize() << std::endl ; 
   
   delete(limo);
   

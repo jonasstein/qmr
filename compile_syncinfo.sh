0ba0d313c877365336f102335606ac3285dc43ba
Jonas Stein <news@jonasstein.de>
2016-10-02 02:29:28 +0200

    merged mkhistogram and syncinfo

--- compile_syncinfo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# dynamic
-export LD_LIBRARY_PATH=.
-g++ syncinfo.cpp -L. -Wl,-R$PWD -o syncinfo -O2 -Wall -Wno-unused-result -std=c++11 
-
--- mkhistogram.cpp
+++ mkhistogram.cpp
@@ -5,12 +5,10 @@
 #include <fstream>      // std::ifstream
 #include <string>     // std::string, std::stoull
 #include <stdio.h>
-#include <byteswap.h> // http://stackoverflow.com/questions/105252/how-do-i-convert-between-big-endian-and-little-endian-values-in-c
-#include "lmfile.hpp"
-//#include "histogram.hpp"
 #include <assert.h>     /* assert */
 #include <limits>
 #include <sys/stat.h> // because boost::filesystem is fragile between different systems, gcc and boost versions
+#include "histogram.hpp"
 
 // true if file exists
 bool fileExists(const std::string& file) {
@@ -19,12 +17,12 @@ bool fileExists(const std::string& file) {
 }
 
 void printhelp(){
-  std::cout << "Usage: mkhistogram <ChDet> <ChSync> <ChSuper> <ChMonitor> <filename>" << std::endl;
+  std::cout << "Usage: mkhistogram <ChDet> <ChSync> <ChSuper> <ChMonitor> <filename> <STARTts> <STOPts> <bins> <mode>" << std::endl;
 }
 
 int main(int argc, char *argv[]){
   
-  if (argc != 8)
+  if (argc != 10)
   {
     std::cout << "Error wrong number of arguments. Stopped." << std::endl;
     printhelp();
@@ -33,23 +31,27 @@ int main(int argc, char *argv[]){
   
   // read parameter  
   std::string ArgThisProgram(argv[0]);
-   int64_t ArgChDet= atoi(argv[1]);
+  int64_t ArgChDet= atoi(argv[1]);
   int64_t ArgChSync=atoi(argv[2]);
   int64_t ArgChSuper=atoi(argv[3]);
   int64_t ArgChMonitor=atoi(argv[4]);
   std::string ArgFilename(argv[5]);
   int64_t ArgSTARTts=atoi(argv[6]);
   int64_t ArgSTOPts=atoi(argv[7]);
+  int64_t ArgBins=atoi(argv[8]);
+  int64_t ArgMode=atoi(argv[9]); // 1=get sync info, 2=generate histogram
   
   std::cerr << "Read file " << ArgFilename << std::endl;  
   std::cerr << "Evaluate between min and max timestamps: (-1 no limits)" << std::endl; 
   std::cerr << ArgSTARTts << std::endl; 
   std::cerr << ArgSTOPts << std::endl; 
+  std::cerr << "Generate histograms with " << ArgBins << " bins" << std::endl;  
   std::cerr << "ChDet:" << ArgChDet << " ChSync:" << ArgChSync << " ChSuper:" << ArgChSuper << " ChMonitor:" << ArgChMonitor << std::endl;
   
   if (ArgSTOPts==-1){
     ArgSTOPts = std::numeric_limits<int64_t>::max();
   }
+
   
   std::ifstream ifs;
   ifs.open(ArgFilename, std::ifstream::in);
@@ -69,12 +71,7 @@ int main(int argc, char *argv[]){
   // if not SUPER read CURRENTts and printf 
   // if SUPER then histo.print(); histo.reset()
   
-  int64_t STARTts;
-  int64_t SYNCts;
-  int64_t SUPERts;
   int64_t CURRENTts;
-  
-  
   int64_t SYNCtsSUM=0;
   int64_t SYNCtsQty=0;
   int64_t SYNCtsMEAN=0;
@@ -90,33 +87,31 @@ int main(int argc, char *argv[]){
   // calculate mean time between SYNC 
   while (!ifs.eof()){
     ifs >> CURRENTts >> TrigID >> DataID >> Data;
-
-    if ((TrigID==7)&&(DataID==ArgChSync)){ //found a SYNC event
+    if ((ArgSTARTts < CURRENTts )&& (CURRENTts < ArgSTOPts) ){
+      
+      if ((TrigID==7)&&(DataID==ArgChSync)){ //found a SYNC event
         if (LastSYNCts>0){
-          std::cout<< CURRENTts - LastSYNCts << std::endl;
-        MaxdSYNCts = std::max(MaxdSYNCts, CURRENTts - LastSYNCts);
-        MindSYNCts = std::max(MindSYNCts, CURRENTts - LastSYNCts);
+          std::cout<< CURRENTts - LastSYNCts << " ns period between " << LastSYNCts << " ns and " << CURRENTts << " ns"<< std::endl;
         }
+        
         SYNCtsSUM+=CURRENTts;
         SYNCtsQty++;
         LastSYNCts=CURRENTts;
-//        std::cout << ifs.tellg() << std::endl;
+        //        std::cout << ifs.tellg() << std::endl;
+      }
+    }}
+    
+    if (SYNCtsQty==0){
+      std::cout << "Zero SYNC signals found!" << std::endl;  
     }
-  }
-  
-  if (SYNCtsQty==0){
-  std::cout << "Zero SYNC signals found!" << std::endl;  
-  }
-  else{
-    SYNCtsMEAN = SYNCtsSUM/SYNCtsQty;
-  std::cout << "# avg SYNC period: " << SYNCtsMEAN << " ns = " << (float)(SYNCtsSUM/SYNCtsQty)/1000000 << " ms " << std::endl;
-  std::cout << "# min SYNC period: " << MindSYNCts << " ns = " << (float)(MindSYNCts)/1000000 << " ms " << std::endl;
-  std::cout << "# max SYNC period: " << MaxdSYNCts << " ns = " << (float)(MaxdSYNCts)/1000000 << " ms " << std::endl;
-  
-  }
-  
-  
-  ifs.close();
-  
-  return(EXIT_SUCCESS);
+    else{
+      SYNCtsMEAN = SYNCtsSUM/SYNCtsQty;
+      std::cout << "# avg SYNC period: " << SYNCtsMEAN << " ns = " << (float)(SYNCtsSUM/SYNCtsQty)/1000000 << " ms " << std::endl;
+      std::cout << "# min SYNC period: " << MindSYNCts << " ns = " << (float)(MindSYNCts)/1000000 << " ms " << std::endl;
+      std::cout << "# max SYNC period: " << MaxdSYNCts << " ns = " << (float)(MaxdSYNCts)/1000000 << " ms " << std::endl;
+      
+    }
+    
+    ifs.close();
+    return(EXIT_SUCCESS);
 }

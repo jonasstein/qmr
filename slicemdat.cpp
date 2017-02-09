40b1eab5c7a2cf7bb63ceb19160f21b9e9e29251
Jonas Stein <jonas.stein@uni-koeln.de>
2017-02-09 15:23:22 +0100

    slicemdat works

--- slicemdat.cpp
+++ slicemdat.cpp
@@ -7,14 +7,14 @@
 
 void printhelp(){
 std::cout << "Usage: slicemdat <filename> <ID> <FROM m> <TO n>" << std::endl;
-std::cout << "Returns the slice between m-th to n-th occurence of ID" 
+std::cout << "Returns the slice between m-th to n-th occurence of ID" << std::endl; 
 }
 
 int main(int argc, char *argv[]){
  
-if (argc != 6)
+if (argc != 5)
   {
-    std::cerr << "Error wrong number of arguments. Stopped." << std::endl;
+    std::cerr << "Error wrong number of arguments. Expected 4, got " << argc-1 << ". Stopped." << std::endl;
     printhelp();
     exit(3);
   }
@@ -24,22 +24,20 @@ if (argc != 6)
   std::string ArgFilename(argv[1]);
   long long ArgID=atol(argv[2]);
   long long ArgFrom=atol(argv[3]);
-  long long ArgTill=atol(argv[4]); 
-  long long ArgMode=atol(argv[5]); 
+  long long ArgTo=atol(argv[4]); 
 
   long long TrigID; 
   long long DataID;
   long long Data;
   long long CURRENTts;
-  long long STARTTIMEts = 0;
-  long long OUTSIDEMode = 1;
-  long long INSIDEMode = 2;
+
+  long long EventCounter = 0;
   
   bool ItsInside = false;
   
   std::cerr << "Read file " << ArgFilename << std::endl;  
   std::cerr << "Filter events after ID=" << ArgID << " events" << std::endl;  
-  std::cerr << "from:" << ArgFrom << " ns till " << ArgTill << " ns" << std::endl;
+  std::cerr << "from:" << ArgFrom << " .. " << ArgTo << std::endl;
   
   std::ifstream ifs;
   ifs.open(ArgFilename, std::ifstream::in);
@@ -51,26 +49,28 @@ if (argc != 6)
   }
 
   while (!ifs.eof()){
+    ItsInside = false;
     ifs >> CURRENTts >> TrigID >> DataID >> Data;
+
     if (ArgID==DataID)
     {
-      STARTTIMEts = CURRENTts;
+	EventCounter++;
+	if (ArgTo==EventCounter) //include end-marker
+	{
+	ItsInside = true;
+	}
     }
-    else
-    {
-      CURRENTts = CURRENTts - STARTTIMEts;
+    
+    // ItsInside is already true, or make true if event is inside the markers
+    ItsInside = ItsInside || ((ArgFrom <= EventCounter) && (EventCounter < ArgTo));
+
+    if (ItsInside)
+	{
+	std::printf("%lld %lld %lld %lld\n", CURRENTts  , TrigID, DataID, Data);
+	}
     }
 
-    ItsInside = (CURRENTts >= ArgFrom) && (CURRENTts <= ArgTill);
-    
-    if ((ArgMode==OUTSIDEMode)&& (~ItsInside))
-    { 
-      std::printf("%lld %lld %lld %lld\n", CURRENTts  , TrigID, DataID, Data);
-    } 
-    else if ((ArgMode==INSIDEMode)&& ItsInside){ 
-      std::printf("%lld %lld %lld %lld\n", CURRENTts  , TrigID, DataID, Data);
-    }
-  }
+  
   ifs.close();
   return(EXIT_SUCCESS);  
 }
